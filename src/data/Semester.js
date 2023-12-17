92bef40cec7e70e9553d3cd046d844d13fc7f34e
Peter Lan <skylan@Peters-MacBook.local>
2023-12-17 02:40:09 -0800

    Add course and semester data

--- src/data/Semester.js
+++ src/data/Semester.js
@@ -1,8 +1,32 @@
 export const SemesterData = [
     {
+        value: "Spring 2024"
+    },
+    {
         value: "Fall 2023"
     },
     {
         value: "Summer 2023"
+    },
+    {
+        value: "Spring 2023"
+    },
+    {
+        value: "Fall 2022"
+    },
+    {
+        value: "Summer 2022"
+    },
+    {
+        value: "Spring 2022"
+    },
+    {
+        value: "Fall 2021"
+    },
+    {
+        value: "Summer 2021"
+    },
+    {
+        value: "Spring 2021"
     }
 ]
--- src/data/Subject.js
+++ src/data/Subject.js
@@ -1,10 +1,50 @@
 export const SubjectData = [
     {
+        name: "Applied Mathematics",
+        symbol: "APMA"
+    },
+    {
+        name: "Applied Physics",
+        symbol: "APPH"
+    },
+    {
+        name: "Biomedical Engineering",
+        symbol: "BMEN"
+    },
+    {
+        name: "Industrial Engineering and Operations Research",
+        symbol: "IEOR"
+    },
+    {
         name: "Computer Science",
         symbol: "COMS"
     },
     {
-        name: "Chemistry",
-        symbol: "CHEM"
+        name: "Chemical Engineering",
+        symbol: "CHEN"
+    },
+    {
+        name: "Civil Engineering and Engineering Mechanics",
+        symbol: "CIEN"
+    },
+    {
+        name: "Earth and Environmental Engineering",
+        symbol: "EAEE"
+    },
+    {
+        name: "Electrical Engineering",
+        symbol: "ELEN"
+    },
+    {
+        name: "Materials Science and Engineering",
+        symbol: "MSAE"
+    },
+    {
+        name: "Mechanical Engineering",
+        symbol: "MECE"
+    },
+    {
+        name: "Data Science",
+        symbol: "STAT"
     }
 ]
